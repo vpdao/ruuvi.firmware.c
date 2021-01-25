7ac7dbb1a3bfb15d3e404d3928c64e206b03dc5d
Phuong Dao <phuong.dao@ngs-sensors.it>
2021-01-25 15:46:46 +0100

    add license for PVS

--- src/app_button.c
+++ src/app_button.c
@@ -1,3 +1,5 @@
+// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 #include "app_button.h"
 #include "app_comms.h"
 #include "app_heartbeat.h"
